c66f8dcd704a14ef9fa2bab61fc986400fc6ec55
Nitin Pathak <nitinpathak24x7@gmail.com>
2026-08-13 20:37:49 +0530

    Time: 0 ms (100%), Space: 41.5 MB (99.12%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        int count=0;
+        for(int i=1;i*i<=n;i++) count++;//Bulbs at perfect square are ON at the end of the day. 
+        return count;
+    }
+}
