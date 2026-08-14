f89325751b32b9fe7dc121193c05e4d6371e7811
Nitin Pathak <nitinpathak24x7@gmail.com>
2026-08-14 12:02:08 +0530

    Time: 2 ms (91.97%), Space: 48 MB (20.66%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+        if(nums[i]==0) count++;
+        else nums[i-count]=nums[i];
+        }
+        while(count>0){
+            nums[nums.length-count]=0;
+            count--;
+        }
+
+    }
+}
